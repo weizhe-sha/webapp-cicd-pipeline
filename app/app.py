30c501aac439225f92d2118cb21a2c5cb7ea6cf8
weizhe-sha <weizhe.sha@gmail.com>
2025-05-15 21:38:36 +1000

    add comments and update cicd yaml file

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
+""" This file handles main application logic. """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """ main function """
     return "Hello, world!"
 
 @app.route("/healthz")
 def health():
+    """ application is healthy """
     return "OK", 200
 
 if __name__ == "__main__":
