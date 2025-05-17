2999a67d45e2cfdc3761d34d7f557da834907b52
weizhe-sha <weizhe.sha@gmail.com>
2025-05-17 18:07:15 +1000

    Add gitignore and terraform scripts

--- tests/test_app.py
+++
@@ -1,8 +0,0 @@
-""" this file handles test cases """
-
-import requests
-
-def test_healthz():
-    """ send a health check request to application """
-    r = requests.get("http://localhost:5000/healthz", timeout=5)
-    assert r.status_code == 200
