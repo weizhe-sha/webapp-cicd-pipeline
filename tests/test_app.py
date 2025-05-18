95c7c63ba1397e50677976ba7ea0a81652ec1efc
weizhe-sha <weizhe.sha@gmail.com>
2025-05-18 20:17:19 +1000

    test

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,12 +11,5 @@ def test_home():
     """ send request to home page"""
     r = requests.get("http://localhost:5000/", timeout=30)
     assert r.status_code == 200
-# def test_healthz():
-#     """ send a health check request to application """
-#     r = requests.get("http://proa-webapp.azurewebsites.net/healthz", timeout=30)
-#     assert r.status_code == 200
 
-# def test_home():
-#     """ send request to home page"""
-#     r = requests.get("http://proa-webapp.azurewebsites.net/", timeout=30)
-#     assert r.status_code == 200
+
