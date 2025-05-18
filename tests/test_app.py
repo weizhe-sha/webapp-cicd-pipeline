77506e004262c9f8bacda8df34598637ca505890
weizhe-sha <weizhe.sha@gmail.com>
2025-05-18 13:36:07 +1000

    test

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,3 +6,8 @@ def test_healthz():
     """ send a health check request to application """
     r = requests.get("http://localhost:5000/healthz", timeout=5)
     assert r.status_code == 200
+
+def test_home():
+    r = requests.get("http://localhost:5000/", timeout=5)
+    assert r.status_code == 200
+    assert b"Hello, world!" in r.data
