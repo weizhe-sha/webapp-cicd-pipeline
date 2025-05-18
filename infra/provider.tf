aae0a366bb9c68ea910e4a886cd7d555bb085a13
weizhe-sha <weizhe.sha@gmail.com>
2025-05-18 14:28:51 +1000

    Configure tf backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 0.14.9"
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "proastg"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 provider "azurerm" {
   subscription_id = "e76f596f-9840-402c-b029-334dc07167a1"
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,11 @@ import requests
 
 def test_healthz():
     """ send a health check request to application """
-    r = requests.get("http://localhost:5000/healthz", timeout=5)
+    r = requests.get("http://proa-webapp.azurewebsites.net:5000/healthz", timeout=5)
     assert r.status_code == 200
 
 def test_home():
     """ send request to home page"""
-    r = requests.get("http://localhost:5000/", timeout=5)
+    r = requests.get("http://proa-webapp.azurewebsites.net:5000/", timeout=5)
     assert r.status_code == 200
     assert b"Hello, world!" in r.data
