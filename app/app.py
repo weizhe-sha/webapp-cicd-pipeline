d0e3095ffff6ab62fb345206c9f58819af470126
weizhe-sha <weizhe.sha@gmail.com>
2025-05-19 12:47:32 +1000

    Fix broken app

--- app/app.py
+++ app/app.py
@@ -8,5 +8,10 @@ def hello():
     ''' This is the main function '''
     return "Hello, world!"
 
+@app.route("/healthz")
+def health():
+    ''' This is the health check function'''
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
