72a9122d2bcb72921f9d59afc64c0ad146073ef1
weizhe-sha <weizhe.sha@gmail.com>
2025-05-19 12:44:10 +1000

    Broken App test

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
+''' This file is a broken app '''
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    ''' This is the main function '''
     return "Hello, world!"
 
 if __name__ == "__main__":
