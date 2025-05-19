43048885a04d3803f6f088b6fa475a052969bad9
weizhe-sha <weizhe.sha@gmail.com>
2025-05-19 12:42:05 +1000

    Broken app

--- app/app.py
+++ app/app.py
@@ -1,18 +1,10 @@
-""" This file handles main application logic. """
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    """ main function """
-    return "Hello Lucy!"
-
-@app.route("/healthz")
-def health():
-    """ application is healthy """
-    return "OK", 200
+    return "Hello, world!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
